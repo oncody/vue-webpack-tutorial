402904c1d48b5d9f620c64b31f82593263188d94
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-03-18 20:46:36 -0600

    Further improving documentation

--- 01-vue-cdn/main.js
+++ 01-vue-cdn/main.js
@@ -1,12 +1,12 @@
+// Create a new vue component
 new Vue({
-    el: '#app', // Here we bind this vue component to the HTML component named 'app' in index.js
+    el: '#app', // Bind this vue component to the HTML component named 'app'
     data: function () { // The 'data' variable should always return a function
         return {
-            // Set the message variable equal to the following value
             message: 'We are including vue from a CDN and this message is coming from the \'main.js\' file'
         }
     },
-    // Everything in this template will be injected inside our HTML component we are bound to.
-    // We are also going to output the value of 'message' that we set in the data section above
+    // Everything in this template will be injected inside the bound HTML component
+    // '{{message}}' will be replaced with the value of message set above
     template: '<div>{{ message }}</div>'
 });
