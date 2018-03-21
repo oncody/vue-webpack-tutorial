69835eaaae0a5c97e3bc22fcdd698cbd53e0af65
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-03-20 19:19:39 -0600

    describing webpack config file

--- 03-html-webpack-plugin/webpack.config.js
+++ 03-html-webpack-plugin/webpack.config.js
@@ -3,14 +3,12 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './main.js',
-    mode: 'development',
+    entry: './main.js', // Entry javascript file
+    mode: 'development', // Whether to run webpack for development or production
     resolve: {
-        alias: {
-            'vue$': 'vue/dist/vue.esm.js' // Necessary for Vue
-        }
+        alias: {'vue$': 'vue/dist/vue.esm.js'} // Necessary for vue
     },
     plugins: [
-        new HtmlWebpackPlugin({template: 'index.html'})
+        new HtmlWebpackPlugin({template: 'index.html'}) // Tell webpack to use our html file and glue to it our javascript files
     ]
 };
