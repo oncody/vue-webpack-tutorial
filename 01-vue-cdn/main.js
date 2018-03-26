e64608cff0d1976ca5ff49bdf0caf004c1429d0c
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-03-26 16:00:18 -0600

    changing output messages

--- 01-vue-cdn/main.js
+++ 01-vue-cdn/main.js
@@ -3,7 +3,7 @@ new Vue({
     el: '#app', // Bind this vue component to the HTML component named 'app'
     data: function () { // The 'data' option should always return a function
         return {
-            message: 'Vue is included from a CDN and working!'
+            message: 'Vue is working from a CDN!'
         }
     },
     // Everything in this template will be injected inside the bound HTML component
