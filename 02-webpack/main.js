951e0eb1dcae3a87cb428ae8501e983de5bd370b
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-03-26 16:00:41 -0600

    changing output messages

--- 02-webpack/main.js
+++ 02-webpack/main.js
@@ -5,7 +5,7 @@ new Vue({
     el: '#app', // Bind this vue component to the HTML component named 'app'
     data: function () { // The 'data' option should always return a function
         return {
-            message: 'Vue is included from NPM, bundled with webpack, and it is working!'
+            message: 'Vue is working with NPM and webpack!'
         }
     },
     template: '<div>{{ message }}</div>' // Everything in this template will be injected inside the bound HTML component
