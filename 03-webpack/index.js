da057ad796c4693a0a7b8c34b35af3a2aae138f6
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-03-28 16:42:25 -0600

    Improving comments

--- 03-webpack/index.js
+++ 03-webpack/index.js
@@ -1,5 +1,7 @@
-// This will import the vue package from NPM node_modules directory
-// Webpack will bundle all of this file's dependencies into this file when webpack runs
+// Webpack allows us to import code from other javascript files into the scope of this javascript file
+// This is different than old javascript days where you globally load your javascript libraries at the start of your web app
+// This import statement will import the vue package from NPM node_modules directory
+// Webpack will bundle all of this file's dependencies into this file during build time when webpack runs
 import Vue from 'vue';
 
 new Vue({
