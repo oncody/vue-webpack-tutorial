a4a00d2f35ed23f6647297408aecc79bbd0392cf
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-06-19 21:59:32 -0600

    adding in other rules that are needed and manually including vue loader plugin

--- 04-vue-loader/webpack.config.js
+++ 04-vue-loader/webpack.config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports = {
     entry: './index.js', // Entry javascript file
     mode: 'development', // Whether to run webpack for development or production
@@ -10,9 +12,23 @@ module.exports = {
     module: {
         rules: [
             {
-                loader: 'vue-loader', // vue-loader allows you to write convenient single-file components
-                test: /\.vue$/ // regex listing which files vue-loader should process
+                test: /\.vue$/, // regex listing which files vue-loader should process
+                loader: 'vue-loader' // vue-loader allows you to write convenient single-file components
+            },
+            {
+                test: /\.js$/, // this will apply to both plain '.js' files and '<script>' blocks in '.vue' files
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,  // this will apply to both plain `.css` files and '<style>' blocks in '.vue' files
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
             }
         ]
-    }
+    },
+    plugins: [
+        new VueLoaderPlugin() // explicitly include the vue loader plugin in your plugins list
+    ]
 };
