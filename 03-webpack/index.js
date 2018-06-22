f4c8ca5142c2612a74b8673123598a979373d760
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-06-22 00:21:10 -0600

    formatting

--- 03-webpack/index.js
+++ 03-webpack/index.js
@@ -1,15 +1,9 @@
-// Webpack allows us to import code from other javascript files into the scope of this javascript file
-// This is different than old javascript days where you globally load your javascript libraries at the start of your web app
-// This import statement will import the vue package from NPM node_modules directory
-// Webpack will bundle all of this file's dependencies into this file during build time when webpack runs
-import Vue from 'vue';
-
 new Vue({
-    el: '#app',
-    data: function () { // The 'data' option should always return a function
-        return {
-            message: 'Vue is working with webpack!'
-        }
-    },
-    template: '<div>{{ message }}</div>'
+  el: '#app',
+  data() {
+    return {
+      message: 'Vue is working with webpack!'
+    };
+  },
+  template: '<div>{{ message }}</div>'
 });
--- 03-webpack/webpack.config.js
+++ 03-webpack/webpack.config.js
@@ -1,9 +1,9 @@
-'use strict';
-
 module.exports = {
-    entry: './index.js', // Entry javascript file
-    mode: 'development', // Whether to run webpack for development or production
-    resolve: {
-        alias: {'vue$': 'vue/dist/vue.esm.js'} // Necessary for vue
+  entry: './index.js',
+  mode: 'development',
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
     }
+  }
 };
