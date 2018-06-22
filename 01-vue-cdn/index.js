335ee144982b73e554ad1e337c777c4a326ac397
Cody Smith <37386486+oncody@users.noreply.github.com>
2018-06-21 20:28:41 -0600

    formatting

--- 01-vue-cdn/index.js
+++ 01-vue-cdn/index.js
@@ -1,12 +1,7 @@
-// Initialize Vue
 new Vue({
-  // Bind this vue component to the HTML component named 'app'
   el: '#app',
   data() {
-    // The 'data' option should always return a function
     return {message: 'Vue is working from a CDN!'};
   },
-  // This entire html template string will be injected
-  // '{{message}}' will be replaced by the value we set above
   template: '<div>{{ message }}</div>'
 });
